ac3b8122b07b8f2b61a0acb29c9a22d4a1d42ac6
Honor Harris <95176411+honorharris@users.noreply.github.com>
2021-12-01 11:41:42 +0000

    Update Working Secret Santa.py

--- Working Secret Santa.py
+++ Working Secret Santa.py
@@ -3,49 +3,18 @@ import csv
 import random 
 import smtplib, ssl
 
-#file with all emails in 
-f#ilename = "C:/Users/honor/Downloads/secret santa.csv"
+#csv file with all emails in 
+#filename = "C:/Users/honor/Downloads/secret santa.csv"
 
 #message to be sent in the emails
 message = """\
-Subject: Your Cretins Secret Santa!
+Subject: Secret Santa!
     
-G'day {name}!
-Welcome to the festive and fun ... Cretins Secret Santa 2021!!
-
-After you signed up to the greatest event in all of human history, the elves are pleased to announce that you have been allocated your Secret Santa!
-However, before we reveal to you who you are lucky enough to have been given, a few reminders!
-
-The limit for gifts is 15 pounds.
-To be confirmed nearer the time on the Crettymen chat, however it is likely that the gift exchange will occur at the Henry Bragg Building during the last week of term. Therefore, please have acquired all gifts by Friday 3rd December 2021 at 12pm. 
-
-And that concludes the reminders! Now onto who your secret santa is...
-
-drumroll please!
-
-
-Congratualtions, your secret santa is {secretsanta}!!
-
-Make sure you buy them a gift that they will despise, so that the rest of us and those in the North Pole can laugh at them! And if you have Nick Surtees, remember that he hates Peppa Pig. 
-    
-    
-That is all! We hope you have a very merry day, week, month and year, that the rest of 2021 is beautiful and bright and full of light and happiness and good times and good memories!
-
-Long Live the Crettymen!
-
-Merry Christmas and Happy New Year! xxxx
-
-
-
-
-
-P.S. What do you call an obnoxious reindeer? 
-hehehe
-... Rude-olph!!
+Message Goes Here
 """
   
 
-#function used when ready to send emails  
+#function used when ready to send emails  - someone hasnt gotten themselves
 def sendemails_func(finallist, lengthoffinal, message):
     '''function for sending emails using the SSL port 465, smtp server and gmail.
     encrypted using ssl'''
@@ -54,7 +23,7 @@ def sendemails_func(finallist, lengthoffinal, message):
     
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com" #host server - gmail
-    sender_email = "secretsantacretins@gmail.com" #email sending from
+    sender_email = "GOOGLE EMAIL HERE" #email sending from
 
     password = input("Type your password and press enter: ")#password for email sending from 
 
@@ -66,7 +35,7 @@ def sendemails_func(finallist, lengthoffinal, message):
             reciever = finallist[x] #finds the name of the person sending email to
             secretsantaname = finallist[x+2] #name of their secret santa
             #TAKE OUT PRINT STATEMENT IF DON'T WANT TO KNOW ALLL THE SECRET SANTA's
-            #print(reciever_email, reciever, secretsantaname) #prints as sending
+            #print(reciever_email, reciever, secretsantaname) #prints as sending - check working
             #next line is what sends the email
             server.sendmail(sender_email, reciever_email, message.format(name = reciever, secretsanta = secretsantaname))
 
@@ -108,12 +77,12 @@ for x in range(0, (length)):
             finallist.append(row)#places above values into a list
 
 #REMOVE FOR SECRET SANTA
-#print(finallist) #prints the final list of name, email, secret santa - not seperate so fancy x+1 and steps need to be used 
+#print(finallist) #prints the final list of name, email, secret santa
 
 
 lengthoffinal = len(finallist) #length of all elements, needed for for loop in sending email 
 
-file.close()    #just in case!
+file.close()    #just in case?
 
 
 sendemails = input("Ready to send email?")
